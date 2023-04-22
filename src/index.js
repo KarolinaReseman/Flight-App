fae3cf109c079a3508d05c681b4ad7e5d101559a
KarolinaReseman <reseman.karolina@gmail.com>
2023-04-23 00:30:18 +0200

    add getWeatherData and showWeatherData

--- src/index.js
+++ src/index.js
@@ -92,6 +92,9 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday' , 'Tuesday' , 'Wedsday' , 'Thursday' , 'Friday' , 'Saturday' ];
 const months = ['January' , 'February' , 'March' , 'April', 'May' , 'June', 'July' , 'August','September', 'October' , 'November', 'December']
 
+const API_KEY = '1527751f4c651ff61028e58ba9a17485';
+
+
 setInterval(() => {
   const time = new Date();
   const month = time.getMonth();
@@ -105,6 +108,63 @@ setInterval(() => {
   timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + '' + `<span id="am-pm">${ampm}</span>` 
   dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]  
 }, 1000);
+
+
+getWeatherData();
+function getWeatherData() {
+  navigator.geolocation.getCurrentPosition((success) => {
+
+    let{latitude, longitude} = success.coords;
+
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json() ).then(data => {
+      console.log(data)
+      showWeatherData(data);
+    })})
+}
+
+showWeatherData();
+function showWeatherData (data) {
+  let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+  timezone.innerHTML = data.timezone;
+  countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
+
+  currentWeatherItemsEl.innerHTML = 
+  `<div class="weather-item">
+    <div>Humidity</div>
+    <div>${humidity}%</div>
+  </div>
+  <div class="weather-item">
+    <div>Pressure</div>
+    <div>${pressure}</div>
+  </div>
+  <div class="weather-item">
+    <div>Wind Speed</div>
+    <div>${wind_speed}</div>
+  </div>`;
+
+  //   let otherDayForcast = ''
+  //   data.daily.forEach((day, idx) => {
+  //     if(idx == 0){
+  //       currentTempEl.innerHTML = `<img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+  //           <div class="other">
+  //             <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+  //             <div class="temp">Night - ${day.temp.night}&#176;C</div>
+  //             <div class="temp">Day - ${day.temp.day}&#176;C</div>
+  //           </div>`
+  //     } else {
+  //       otherDayForcast += `<div class="weather-forecast-item">
+  //         <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+  //           <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+  //           <div class="temp">Night - ${day.temp.night}&#176;C</div>
+  //           <div class="temp">Day - ${day.temp.day}&#176;C</div>
+  //         </div>`
+  //     }
+  //   })
+
+
+  //   weatherForecastEl.innerHTML = otherDayForcast;
+};
 
 function sprawdzLot() {
   var poczatkowa = document.getElementById("start").value;
